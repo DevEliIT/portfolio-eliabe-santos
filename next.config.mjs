b79686462bf72922c5f6af7252c53f604fe31cc6
Eliabe dos Santos Oliveira <eliabe.s.o36@gmail.com>
2026-07-21 20:14:52 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,12 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: '**.supabase.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'images.unsplash.com',
         port: '',
         pathname: '/**',
